8ad220cb9d572989778c3bac5a32ac8ce5efe5f3
Billy Hao <billyhao12@gmail.com>
2024-05-26 10:37:08 -0700

    add new files to `CMakeLists.txt`

--- Telegram/SourceFiles/boxes/create_event_box.cpp
+++ Telegram/SourceFiles/boxes/create_event_box.cpp
@@ -5,7 +5,6 @@ the official desktop application for the Telegram messaging service.
 For license and copyright information please follow this link:
 https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
-#include "C:/TBuild/tdesktop-fork/out/Telegram/CMakeFiles/Telegram.dir/Debug/cmake_pch.hxx"
 #include "boxes/create_event_box.h"
 
 #include "base/call_delayed.h"
